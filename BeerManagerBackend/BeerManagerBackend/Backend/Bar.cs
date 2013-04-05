9f8ff48a21e86f97f4a236425b2fcec02a5bfa32
labday <labday@lab.org>
2013-04-05 15:16:47 +0300

    Changed as chat testing.

---
+++ BeerManagerBackend/BeerManagerBackend/Backend/Bar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+
+namespace BeerManagerBackend
+{
+    public class Bar : Hub
+    {
+        public void Send(string message, string fromUser)
+        {
+            // Call the addMessage method on all clients            
+            Clients.All.addMessage(DateTime.Now + " " + fromUser + ":   " + message);
+        }
+    }
+}
